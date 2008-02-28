da566b94914b2f49e9a17f1487b8e4d6fde212c5
Eric Mertens <emertens@localhost.localdomain>
2008-02-28 00:37:39 -0800

    scrub cbits of unused functions

--- cbits/python-local.c
+++ cbits/python-local.c
@@ -1,10 +1,3 @@
 #include <python2.5/Python.h>
 
 void py_decref(PyObject *o) { Py_DECREF(o); }
-void py_incref(PyObject *o) { Py_INCREF(o); }
-PyObject *py_return_true() { Py_RETURN_TRUE; }
-PyObject *py_return_false() { Py_RETURN_FALSE; }
-PyObject *py_unicode_decode_utf8(const char *a, int b) { return PyUnicode_DecodeUTF8(a,b,"strict"); }
-PyObject *py_unicode_as_utf8(PyObject *a) { return PyUnicode_AsUTF8String(a); }
-int py_iter_check(PyObject *a) { return PyIter_Check(a); }
-PyObject *py_none = Py_None;
--- include/python-local.h
+++ include/python-local.h
@@ -2,13 +2,6 @@
 #define __PYTHON_LOCAL_H
 #include <python2.5/Python.h>
 
-extern void py_incref(PyObject *o);
 extern void py_decref(PyObject *o);
-extern PyObject* py_return_true();
-extern PyObject* py_return_false();
-extern PyObject* py_unicode_decode_utf8(const char *a, int s);
-extern PyObject *py_unicode_as_utf8(PyObject *a);
-extern int py_iter_check(PyObject *a);
-extern PyObject *py_none;
 
 #endif
